630ecada342f2a90c30ed07fa7e333e1069cc2ca
Andy <andyquacht@gmail.com>
2024-12-01 02:01:58 -0800

    i started with the bar graphs

--- Calculation.asm
+++ Calculation.asm
@@ -15,18 +15,87 @@ ef_car: .double 0.3
 ef_carpool: .double 0.3
 zero_value: .double 0.0  # Default value for non-motorized modes
 
+
+# set display to:
+#	Pixels width and height to 4x4
+#	Display width and height to 256x256
+#	Base address = 0x10010000
+# This will make our screen width 64x64 (256/4 = 64)
+# 64 * 64 * 4 = 16384 required bytes
+
+display:	.space 16384
+
+define:
+# screen information
+	.eqv PIXEL_SIZE 4
+	.eqv WIDTH 64
+	.eqv HEIGHT 64
+	.eqv DISPLAY 0x10000000
+
+# colors
+	.eqv	GRAY 	0x00A0A0A0
+	.eqv	WHITE 	0x00FFFFFF
+	.eqv	RED	0x00FF0000
+	.eqv	YELLOW	0x00FFFF00
+	.eqv	GREEN	0x0000FF00
+	.eqv	BLUE	0x000000FF
+
+
 .text
 .globl main
 
 main:
-    # Initialize stack pointer
-    li $sp, 0x7fffeff8       
+# set the frame pointer to the beginning of the stack
+    move $fp, $sp       
+    
+    
+	li $a0, WHITE		# set white as the background color
+	jal backgroundColor	# color the background
+	
+	
+	li $a0, 1		# set first bar starting x position to x = 1
+	li $a1, 10		# set first bar starting x position to x = 10
+	li $a2, 64		# set first bar height to 64
+	li $a3, GRAY		# set color to gray
+	jal drawBar		# draw bar
 
+	li $a0, 14		# set first bar starting x position to x = 14
+	li $a1, 23		# set first bar starting x position to x = 23
+	li $a2, 64		# set first bar height to 64
+	li $a3, GRAY		# set color to gray
+	jal drawBar		# draw bar
+	
+	
+	li $a0, 27		# set first bar starting x position to x = 27
+	li $a1, 36		# set first bar starting x position to x = 36
+	li $a2, 64		# set first bar height to 64
+	li $a3, GRAY		# set color to gray
+	jal drawBar		# draw bar
+	
+	li $a0, 40		# set first bar starting x position to x = 40
+	li $a1, 49		# set first bar starting x position to x = 49
+	li $a2, 64		# set first bar height to 64
+	li $a3, GRAY		# set color to gray
+	jal drawBar		# draw bar
+	
+	
+	li $a0, 53		# set first bar starting x position to x = 53
+	li $a1, 62		# set first bar starting x position to x = 62
+	li $a2, 64		# set first bar height to 64
+	li $a3, GRAY		# set color to gray
+	jal drawBar		# draw bar
+
+	
+	
     # Input weekday transportation data
     jal handle_weekday_transportation
 
     # Display weekday transportation emissions
     jal display_weekday_emissions
+
+
+
+
 
     # Exit program
     li $v0, 10
@@ -145,3 +214,99 @@ display_weekday_emissions:
     lw $ra, 0($sp)           # Restore return address
     addiu $sp, $sp, 4        # Deallocate stack space
     jr $ra                   # Returning control to main
+    
+
+
+
+
+
+# draws the background color
+# precondition: $a0 is set the color
+backgroundColor:
+	
+	li $s1, DISPLAY		# The first pixel on the display
+		# set s2 = the last memory address of the display
+	li $s2, WIDTH
+	mul $s2, $s2, HEIGHT
+	mul $s2, $s2, 4		# word
+	add $s2, $s1, $s2
+
+backgroundLoop:
+	sw $a0, 0($s1)
+	addiu $s1, $s1, 4
+	ble $s1, $s2, backgroundLoop
+	
+	jr $ra
+
+
+
+# draws pixels
+# preconditions
+#	$a0 = x
+#	$a1 = y
+#	$a2 = color
+
+draw_pixel:
+	
+	addi $sp, $sp, -8		# make room on the stack for 2 words ($ra, $fp)
+	sw $fp, 4($sp)
+	addi $fp, $sp, 4	# move the $fp to the boginning of this stack frame
+	sw $ra, -4($fp)
+
+	# $s1 = address = DISPLAY + 4 * (x + (y * WIDTH))
+	mul $s1, $a1, WIDTH	# s1 = (y * WIDTH)
+	add $s1, $s1, $a0	# (x + s1)
+	mul $s1, $s1, 4		# word (4 bytes)
+	sw $a2, DISPLAY($s1)
+	
+
+	lw $ra, -4($fp)
+	lw $fp, 0($fp)
+	addi $sp, $sp, 8		# pop off the stack
+
+	jr $ra
+	
+
+
+
+
+# draws vertical bars
+# preconditions
+#	$a0 = x (starting)
+#	$a1 = x (ending)
+#	$a2 = height
+#	$a3 = color
+drawBar:
+	addi $sp, $sp, -8		# make room on the stack for 2 words ($ra, $fp)
+	sw $fp, 4($sp)		# store frame pointer
+	addi $fp, $sp, 4	# move the $fp to the beginning of this stack frame
+	sw $ra, -4($fp)		# store return address
+
+	li $s0, 65		# set y offset to bottom
+	move $s3, $a0		# The starting x position
+	move $s4, $a1		# The ending x position
+	sub $s5, $s0, $a2	# set s5 to height of bar
+	move $s6, $s3		# set s6 to reset s3 position
+
+	move $a2, $a3		# set color
+
+barLoopHorizontal:
+	bgt $s3, $s4, barLoop	# if s3 > s4
+	move $a0, $s3		# set x offset from s3
+	move $a1, $s0		# set y offset from s4
+	jal draw_pixel		# draw pixel
+	
+	addi $s3, $s3, 1	# move x one unit over
+	j barLoopHorizontal
+
+barLoop:
+	blt $s0, $s5, exit	# if s0 < s5
+	move $s3, $s6		# The starting x position
+	subi $s0, $s0, 1	# move y one position up
+	j barLoopHorizontal
+exit:	
+	lw $ra, -4($fp)			# restore return address
+	lw $fp, 0($fp)			# restore frame pointer
+	addi $sp, $sp, 8		# pop off the stack
+	
+	jr $ra
